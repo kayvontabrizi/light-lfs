5bc854d55ded325eb6780bd123cb8cca3db0fa5f
Kayvon Tabrizi <kayvon.r.tabrizi@gmail.com>
2018-01-11 00:13:25 -0500

    Initial commit for Light LFS management. Includes a git filter, which converts large files into hashes for storage and performs syncing to a specified local path. Also includes Automator services which should be used to mark and add (favorite) a file, thus also downloading it.
    Notes: Sync operations are locally logged. Filter will be separated into two parts; (1) a hashing filter, and (2) a separate syncing system, to be called after favorites have been added. This syncing system will attempt a git commit, and then compare the local directory contents with a list of git-tracked files in order to determine what to download and what to delete.

---
+++ light_lfs_add.sh
@@ -0,0 +1,36 @@
+# track number of subdirectories
+MIN_DIRS=10000
+
+# loop through input files and folders
+for f in "$@"; do
+	# extract directory and determine number of dirs
+	DIR=$(dirname "$f")
+	DIRS=$(grep -o "/" <<< "$DIR" | wc -l)
+
+	# save most root directory
+	if [ $DIRS -lt $MIN_DIRS ]; then
+		SET_DIR=$DIR
+		MIN_DIRS=$DIRS
+	fi
+done
+
+# set current directory to the most root file directory
+cd "$SET_DIR"
+
+# exit if current directory is not git repository
+if ! ( [ -d .git ] || git rev-parse --git-dir > /dev/null 2>&1 ); then
+	exit 0
+fi
+
+# exit if current directory has not configured Light LFS
+if [ -z $(git config filter.light_lfs.localpath) ]; then exit 0; fi;
+
+# enable tag command
+export PATH="/usr/local/bin/:$PATH"
+
+# loop through input files and folders
+for f in "$@"; do
+	# git add and tag each file and folder
+	git add "$f"
+	tag -a Blue -R "$f"
+done
